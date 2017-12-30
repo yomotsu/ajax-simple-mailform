3cbf351f1460e3e5038c98c604cd20500c22ad65
yomotsu <oyamada@pxgrid.com>
2017-12-30 21:17:06 +0900

    firstcommit

---
+++ sendmail.js
@@ -0,0 +1,49 @@
+( function () {
+
+	const $form = document.getElementById( 'mailform' );
+	$form.addEventListener( 'submit', function( event ) {
+
+		event.preventDefault();
+		const formData = new FormData();
+		const nameList = [];
+
+		Array.prototype.forEach.call( $form.elements, function( $input ) {
+
+			if ( ! $input.name || formData.has( $input.name ) ) return;
+
+			formData.append( $input.name, $form.elements[ $input.name ].value );
+
+		} );
+
+		formData.append( 'formsecret', location.host );
+
+		const xhr = new XMLHttpRequest();
+		xhr.open( 'POST', $form.action, true );
+		xhr.onload = function () {
+
+			const json = JSON.parse( xhr.responseText );
+
+			if ( json.state === 'OK' ) {
+
+				// alert( 'Your email has been sent successfully.' );
+				alert( 'お問い合わせを受け付けました。' );
+				$form.reset();
+				return;
+
+			}
+
+			if ( json.state === 'Error' ) {
+
+				alert( '入力内容にエラーがあります。入力内容をご確認ください。' );
+				// alert( 'Faild. Some field(s) does not appear to be valid. Please check once again.' );
+				return;
+
+			}
+
+		};
+
+		xhr.send( formData );
+
+	} );
+
+} )();
---
+++ sendmail.php
@@ -0,0 +1,112 @@
+<?php
+
+$REQUIRED_ITEMS = [
+	"name",
+	"email"
+];
+
+$to       = "admin@yomotsu.net";
+$from     = "no-reply@example.com";
+$subject  = " | ".$_SERVER[ "HTTP_HOST" ];
+$template = 
+// "Thank you for contacting us. We will be in touch with you very soon.\n".
+// "We have received the below message.\n".
+"お問い合わせありがとうございます。\n".
+"以下のお問い合わせ内容で、お問い合わせを受け付けました。\n".
+"内容を確認のうえ、ご返信いたします。\n".
+
+"------------------------------\n".
+"## お名前\n".
+$_POST[ "name" ]."\n".
+"\n".
+"## メールアドレス\n".
+$_POST[ "email" ]."\n".
+"\n".
+"## 選択肢1\n".
+$_POST[ "radio1" ]."\n".
+"\n".
+"## お問い合わせ内容\n".
+$_POST[ "comment" ]."\n".
+"\n".
+"------------------------------\n".
+"\n".
+USERINFO()."\n";
+
+// -----------------------------------------------------------------------------
+
+header( "content-type: application/json; charset=utf-8" );
+
+mb_language( "Japanese" );
+mb_internal_encoding( "UTF-8" );
+$headers = "From: " . $from . "\r\n";
+$result = array(
+	"errors" => array(),
+	"state" => 0
+);
+
+// bot対策: formsecretの値のチェック
+if ( ! $_SERVER[ "HTTP_HOST" ] === $_POST[ "formsecret" ] ) {
+
+	$result[ "state" ] = "Not Acceptable";
+	echo json_encode( $result );
+	exit;
+
+}
+
+// echo $template;
+
+// 必須項目が空になっていないかのチェック
+foreach ( $REQUIRED_ITEMS as $requiredItemKey ) {
+
+	if ( empty( $_POST[ $requiredItemKey ] ) ) {
+
+		array_push( $result[ "errors" ], $requiredItemKey );
+
+	}
+
+}
+
+// emailパターンチェック
+if (
+	! preg_match( "/^[\w\-\.]+\@[\w\-\.]+\.([a-z]+)$/", $_POST[ "email" ] ) &&
+	in_array( "email", $result[ "errors" ] )
+) {
+
+	array_push( $result[ "errors" ], "email" );
+
+}
+
+$hasError = count( $result[ "errors" ] ) > 0;
+
+// エラーがあればJSONを出力して終わり
+if ( $hasError ) {
+
+	$result[ "state" ] = "Error";
+	echo json_encode( $result );
+	exit;
+
+}
+
+if (
+	mb_send_mail( $to,               $subject, $template, $headers ) &&
+	mb_send_mail( $_POST[ "email" ], $subject, $template, $headers )
+) {
+
+	$result[ "state" ] = "OK";
+	echo json_encode( $result );
+	exit;
+
+} else {
+
+	$result[ "state" ] = "Failed";
+	echo json_encode( $result );
+	exit;
+
+}
+
+
+function USERINFO(){
+
+	return @gethostbyaddr( $_SERVER[ "REMOTE_ADDR" ] ) . "\n" . $_SERVER[ "HTTP_USER_AGENT" ];
+
+}
